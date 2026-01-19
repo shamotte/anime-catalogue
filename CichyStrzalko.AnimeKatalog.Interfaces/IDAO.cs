2b0bf945f04e5a1efebfc14dab33ea955759569b
shamotte <123325697+shamotte@users.noreply.github.com>
2026-01-19 15:21:45 +0100

    aaaa

--- CichyStrzalko.AnimeKatalog.Interfaces/IDAO.cs
+++ CichyStrzalko.AnimeKatalog.Interfaces/IDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CichyStrzalko.AnimeKatalog.INTERFACES;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,25 @@ using System.Threading.Tasks;
 
 namespace CichyStrzalko.AnimeKatalog.Interfaces
 {
-    internal interface IDAO
+    public interface IDAO
     {
+        //GETERS
+        IEnumerable<IStudio> GetAllStudios();
+        //IEnumerable<IProduct> GetAllProducts();
+        IEnumerable<IAnime> GetAllAnimes();
+
+        //CREATES
+        IStudio CreateNewStudio(IStudio studio);
+        //IProduct CreateNewProduct(IProduct product);
+        IAnime CreateNewCPU(IAnime anime);
+
+        //DELETES
+        void DeleteStudio(int studioId);
+        //void DeleteProduct(int productId);
+        void DeleteAnime(int animeId);
+
+        //UPDATES
+        void UpdateStudio(IStudio studio);
+        void UpdateCPU(IAnime anime);
     }
 }
