3d05ffb494b5da2828447dc6b5660a1761f27c03
shamotte <123325697+shamotte@users.noreply.github.com>
2026-01-19 14:23:07 +0100

    aaaaaaaa

---
+++ CichyStrzalko.AnimeKatalog.Core/Enums.cs
@@ -0,0 +1,18 @@
+﻿namespace CichyStrzalko.AnimeKatalog.Core
+{
+    [Flags]
+    public enum Genre
+    {
+        Comedy = 1 << 0,
+        Horror = 1 << 1,
+        SliceOfLife = 1 << 2,
+        Action = 1 << 3,
+        Isekai = 1 << 4,
+        Drama = 1 << 5,
+        Cryminal = 1 << 6,
+        ScienceFiction = 1 << 7,
+        Fantasy = 1 << 8,
+
+
+    }
+}
---
+++ CichyStrzalko.AnimeKatalog.Interfaces/Class1.cs
@@ -0,0 +1,24 @@
+﻿using CichyStrzalko.AnimeKatalog.Core;
+
+
+namespace CichyStrzalko.AnimeKatalog.Interfaces
+{
+    public interface Anime
+    {
+        int Id { get; set; }
+
+        string Name { get; set; }
+
+        string Studio { get; set; }
+
+        int Episodes {  get; set; }
+
+        string Premiere { get; set; }
+
+        Genre Genre { get; set; }
+
+        
+
+
+    }
+}
