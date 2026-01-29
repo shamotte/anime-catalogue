3da94eba0c9133d103c3271936ed98b37ba9285e
igorekkedziorek <126686402+igorekkedziorek@users.noreply.github.com>
2026-01-29 13:39:00 +0100

    Commands start

--- CichyStzalko.AnimeKatalog.UI/MainWindow.xaml.cs
+++ CichyStzalko.AnimeKatalog.UI/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using CichyStrzalko.AnimeKatalog.UI.ViewModels.Lists;
 using CichyStrzalko.AnimeKatalog.BL;
 using Microsoft.Extensions.Configuration;
 using System.Configuration;
+using System.Windows.Xps;
 
 namespace CichyStrzalko.AnimeKatalog.UI
 {
@@ -22,10 +23,30 @@ namespace CichyStrzalko.AnimeKatalog.UI
     public partial class MainWindow : Window
     {
         public AnimeListViewModel AnimeList { get; set; } = new AnimeListViewModel();
+        private ViewModels.AnimeViewModel SelectedAnime = null;
         public CharacterListViewModel CharacterList { get; set; } = new CharacterListViewModel();
+        private ViewModels.CharacterViewModel SelectedCharacter = null;
         public StudioListViewModel StudioList { get; set; } = new StudioListViewModel();
         private IConfiguration Configuration;
+        private ViewModels.StudioVievModel StudioVievModel = null;
         private BL.BL _BL;
+
+        public ICommand AddCommandStudio { get; }
+        public ICommand EditCommandStudio { get; }
+        public ICommand DeleteCommandStudio { get; }
+
+
+
+        public ICommand AddCommandAnime { get; }
+        public ICommand EditCommandAnime { get; }
+        public ICommand DeleteCommandAnime { get; }
+
+
+        public ICommand AddCommandCharacter { get; }
+        public ICommand EditCommandCharacter { get; }
+        public ICommand DeleteCommandCharacter { get; }
+
+        //TODO: rzeczywiście jakieś funkcje do edycji czy coś
         public MainWindow()
         { 
             //TODO: real configuration here
--- CichyStzalko.AnimeKatalog.UI/ViewModels/Elements/StudioViewModel.cs
+++ CichyStzalko.AnimeKatalog.UI/ViewModels/Elements/StudioViewModel.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using CichyStrzalko.AnimeKatalog.Interfaces;
 
-namespace CichyStrzalko.AnimeKatalog.UI.ViewModels.Elements
+namespace CichyStrzalko.AnimeKatalog.UI.ViewModels
 {
     public class StudioVievModel : INotifyPropertyChanged
     {
