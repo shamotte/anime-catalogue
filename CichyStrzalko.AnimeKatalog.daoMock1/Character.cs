9dd0a94c3454796a5d2d30bc17eea1304e453ba0
shamotte <123325697+shamotte@users.noreply.github.com>
2026-01-29 20:34:15 +0100

    aaaaaa

--- CichyStrzalko.AnimeKatalog.daoMock1/Character.cs
+++ CichyStrzalko.AnimeKatalog.daoMock1/Character.cs
@@ -14,5 +14,6 @@ namespace CichyStrzalko.AnimeKatalog.daoMock1
         public int Id { get; set; }
         public string Name { get; set; }
         public IAnime Anime { get; set; }
+        public string ImageFile { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
