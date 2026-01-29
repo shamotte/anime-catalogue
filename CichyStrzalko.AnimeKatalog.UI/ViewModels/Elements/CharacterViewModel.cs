a6b0387296d5ab63ce50e6d0573007d475623625
igorekkedziorek <126686402+igorekkedziorek@users.noreply.github.com>
2026-01-30 00:42:07 +0100

    ww

--- CichyStrzalko.AnimeKatalog.UI/ViewModels/Elements/CharacterViewModel.cs
+++ CichyStrzalko.AnimeKatalog.UI/ViewModels/Elements/CharacterViewModel.cs
@@ -11,26 +11,11 @@ namespace CichyStrzalko.AnimeKatalog.UI.ViewModels
 {
     public partial class CharacterViewModel : ObservableObject
     {
-        //public event PropertyChangedEventHandler? PropertyChanged;
         [ObservableProperty]
         private ICharacter character;
         public CharacterViewModel(ICharacter character) {
         this.character = character;
         }
 
-        //public int Id { get => Character.Id;}
-        
-        //public string Name { get => Character.Name; set {
-        //        Character.Name = value;
-        //        SendPropertyChanged(nameof(Name));
-        //    }
-        //}
-        //public IAnime Anime {  get => Character.Anime;
-        //set {
-        //    Character.Anime = value;
-        //    SendPropertyChanged(nameof(Anime));
-        //    }
-        //}
-        //private void SendPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
--- CichyStrzalko.AnimeKatalog.UI/ViewModels/Elements/StudioViewModel.cs
+++ CichyStrzalko.AnimeKatalog.UI/ViewModels/Elements/StudioViewModel.cs
@@ -11,29 +11,11 @@ namespace CichyStrzalko.AnimeKatalog.UI.ViewModels
 {
     public partial class StudioViewModel : ObservableObject
     {
-        //public event PropertyChangedEventHandler? PropertyChanged;
 
         [ObservableProperty]
         private IStudio studio;
         public StudioViewModel(IStudio studio) { 
             this.studio = studio;
         }
-
-        //public int Id { get => Studio.Id;}
-
-        //public string Name { get => Studio.Name;
-        //    set { Studio.Name = value;
-        //    SendPropertyChanged(nameof(Name));
-        //    }
-        //}
-
-        //public string Address { 
-        //    get => Studio.Address;
-        //    set {
-        //        Studio.Address = value;
-        //        SendPropertyChanged(nameof(Address));
-        //    }
-        //}
-        //private void SendPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
