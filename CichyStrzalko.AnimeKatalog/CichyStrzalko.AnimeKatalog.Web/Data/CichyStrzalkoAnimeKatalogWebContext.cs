3c0483cabfe116b88689d2f05c971f6d65e45a77
igorekkedziorek <126686402+igorekkedziorek@users.noreply.github.com>
2026-02-05 11:13:11 +0100

    Character controlla

--- CichyStrzalko.AnimeKatalog/CichyStrzalko.AnimeKatalog.Web/Data/CichyStrzalkoAnimeKatalogWebContext.cs
+++ CichyStrzalko.AnimeKatalog/CichyStrzalko.AnimeKatalog.Web/Data/CichyStrzalkoAnimeKatalogWebContext.cs
@@ -16,5 +16,6 @@ namespace CichyStrzalko.AnimeKatalog.Web.Data
 
         public DbSet<CichyStrzalko.AnimeKatalog.Web.Models.Studio> Studio { get; set; } = default!;
         public DbSet<CichyStrzalko.AnimeKatalog.Web.Models.Anime> Anime { get; set; } = default!;
+        public DbSet<CichyStrzalko.AnimeKatalog.Web.Models.Character> Character { get; set; } = default!;
     }
 }
